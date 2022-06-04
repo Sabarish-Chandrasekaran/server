acde4a57cd97835e4d22dd2e0bf2c891f8b3ee2b
Sabarish Chandrasekaran <sabarish27k6@gmail.com>
2022-06-04 14:39:37 +0530

    changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const cors = require("cors");
 const connectDB = require("./config/config");
 require("colors");
 const morgan = require("morgan");
@@ -14,11 +14,12 @@ connectDB();
 const app = express();
 
 //middlewares
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
 //route
-app.use("/api/pizzas", require("./routes/pizzaRoute"))
+app.use("/api/pizzas", require("./routes/pizzaRoute"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.get("/", (req, res) => {
   res.send("<h1>Hello From Node Server vai nodemon</h1>");
@@ -27,7 +28,7 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(
-    `Server Running On ${process.env.NODE_ENV} mode on port no ${process.env.PORT}`
+    `Server Running On ${process.env.NODE_ENV} mode on port number ${process.env.PORT}`
       .bgMagenta.white
   );
 });
